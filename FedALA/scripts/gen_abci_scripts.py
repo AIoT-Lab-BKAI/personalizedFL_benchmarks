f56ed7330f8a09f6b6e335e1810a1fb9261652c2
sudo-u-Hung-NN <hung.nn184118@sis.hust.edu.vn>
2023-01-13 22:01:59 +0700

    Fix scripts

--- FedALA/scripts/gen_abci_scripts.py
+++ FedALA/scripts/gen_abci_scripts.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 
-dataset = "mnist"
+dataset = "cifar10"
 noniid = "dir_1_sparse"
 N = 20
 K = 5
@@ -17,26 +17,26 @@ if not Path(f"./{dataset}/{N}_clients").exists():
 
 
 header_text = "\
-# #!/bin/bash\n\
-# #$ -cwd\n\
-# #$ -l rt_G.small=1\n\
-# #$ -l h_rt=36:00:00\n\
-# #$ -o /home/aaa10078nj/Federated_Learning/Hung_perFL/logs/mnist/$JOB_NAME_$JOB_ID.log\n\
-# #$ -j y\n\n\
-# source /etc/profile.d/modules.sh\n\
-# module load gcc/11.2.0\n\
-# module load openmpi/4.1.3\n\
-# module load cuda/11.5/11.5.2\n\
-# module load cudnn/8.3/8.3.3\n\
-# module load nccl/2.11/2.11.4-1\n\
-# module load python/3.10/3.10.4\n\
-# source ~/venv/pytorch1.11+horovod/bin/activate\n\n\
-# LOG_DIR=\"/home/aaa10078nj/Federated_Learning/Hung_perFL/logs/mnist/$JOB_NAME_$JOB_ID\"\n\
-# rm -r ${LOG_DIR}\n\
-# mkdir ${LOG_DIR}\n\n\
-# #Dataset\n\
-# DATA_DIR=\"$SGE_LOCALDIR/$JOB_ID/\"\n\
-# cp -r ./easyFL/benchmark/mnist/data ${DATA_DIR}\n\n\
+#!/bin/bash\n\
+#$ -cwd\n\
+#$ -l rt_G.small=1\n\
+#$ -l h_rt=36:00:00\n\
+#$ -o /home/aaa10078nj/Federated_Learning/Hung_perFL/logs/cifar10/$JOB_NAME_$JOB_ID.log\n\
+#$ -j y\n\n\
+source /etc/profile.d/modules.sh\n\
+module load gcc/11.2.0\n\
+module load openmpi/4.1.3\n\
+module load cuda/11.5/11.5.2\n\
+module load cudnn/8.3/8.3.3\n\
+module load nccl/2.11/2.11.4-1\n\
+module load python/3.10/3.10.4\n\
+source ~/venv/pytorch1.11+horovod/bin/activate\n\n\
+LOG_DIR=\"/home/aaa10078nj/Federated_Learning/Hung_perFL/logs/cifar10/$JOB_NAME_$JOB_ID\"\n\
+rm -r ${LOG_DIR}\n\
+mkdir ${LOG_DIR}\n\n\
+#Dataset\n\
+DATA_DIR=\"$SGE_LOCALDIR/$JOB_ID/\"\n\
+cp -r ./easyFL/benchmark/cifar10/data ${DATA_DIR}\n\n\
 "
 
 formated_command = "\
